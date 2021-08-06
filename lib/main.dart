a9de4dbcc54dcb9d376a3b19bba5818b3a5bf067
mrcchef <201951003@iiitvadodara.ac.in>
2021-08-07 00:37:11 +0530

    Google auth added

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+void main() {
   runApp(MyApp());
 }
 

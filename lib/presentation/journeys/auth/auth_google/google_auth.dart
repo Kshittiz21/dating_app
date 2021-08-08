af63d1d2a781a3c6590657b916e84b667d48c173
mrcchef <201951003@iiitvadodara.ac.in>
2021-08-08 14:29:50 +0530

    google sign up improved

--- lib/presentation/journeys/auth/auth_google/google_auth.dart
+++ lib/presentation/journeys/auth/auth_google/google_auth.dart
@@ -1,5 +1,6 @@
 import 'package:dating_app/domain/entities/user_data.dart';
 import 'package:dating_app/domain/entities/user_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:provider/provider.dart';
@@ -11,16 +12,35 @@ class GoogleAuth with ChangeNotifier {
   String? displayName;
 
   void login(BuildContext context) async {
-    googleSignInAccount = await _googleAuth.signIn();
-    email = googleSignInAccount?.email;
-    displayName = googleSignInAccount?.displayName;
+    // googleSignInAccount = await _googleAuth.signIn();
+    // email = googleSignInAccount?.email;
+    // displayName = googleSignInAccount?.displayName;
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+    final GoogleSignIn _googleSignIn = GoogleSignIn(
+      scopes: ['email'],
+    );
+    final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser!.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    final User? user = (await _auth.signInWithCredential(credential)).user;
+    displayName = user!.displayName;
+    email = user.email;
+    print(user.displayName);
+    print(user.email);
+    user.uid;
+    print(user.emailVerified);
+
     Provider.of<UserModel>(context, listen: false).updateEmail(email);
-    // notifyListeners();
   }
 
   void logout(BuildContext context) async {
     googleSignInAccount = await _googleAuth.signOut();
     Provider.of<UserModel>(context, listen: false).updateEmail(null);
-    // notifyListeners();
   }
 }
